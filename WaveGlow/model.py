4cb02350d544757427469d9f2877783f87c69e43
Crie <crosssceneofwindff@gmail.com>
2018-12-31 13:10:08 +0900

    update

--- WaveGlow/model.py
+++ WaveGlow/model.py
@@ -1,40 +1,14 @@
 import chainer
-import chainer.links as L
 import chainer.functions as F
-from chainer import cuda,Chain,initializers
-import numpy as np
+import chainer.links as L
+import numpy
 
-xp=cuda.cupy
-cuda.get_device(0).use()
+def _normalize(W):
+    xp = chainer.cuda.get_array_module(W)
+    g = xp.sqrt(xp.sum(W ** 2, axis=(1, 2), keepdims=True))
+    v = W / (g + 1e-16)
+    return g, v
 
-class InvertibleConvolution(Chain):
-    def __init__(self,in_ch):
-        W=xp.linalg.qr(xp.random.normal(0,1,(in_ch,in_ch)))[0].astype(xp.float32)
-        if xp.linalg.det(W):
-            W[:,0]*=-1
-        W=W.reshape(W.shape+(1,))
-        super(IntertibleConvolution,self).__init__()
-
-        with self.init_scope():
-            self.w=chainer.Parameter(W)
-
-    def invW(self):
-        return F.expand_dims(F.inv(self.w[:,0]), axis=2)
-
-    def __call__(self,x):
-        conv=F.convolution_nd(x,self.w)
-        logdet=F.log(F.det(self.w[...,0]))
-
-        return conv, logdet
-
-    def backward(self,x):
-        return F.convolution_nd(x,self.invW)
-
-def normalize(x):
-    g=xp.sqrt(F.sum(x**2,axis=(1,2),keepdims=True)
-    w=x/(g+1e-16)
-
-    return w,g
 
 def weight_norm(link):
     assert hasattr(link, 'W')
@@ -57,9 +31,8 @@ def weight_norm(link):
         args.link.v.array[...] = v
         args.link.W = _W(args.link)
 
-    g, v = normalize(link.W.array)
+    g, v = _normalize(link.W.array)
     del link.W
-
     with link.init_scope():
         link.g = chainer.Parameter(g)
         link.v = chainer.Parameter(v)
@@ -69,181 +42,218 @@ def weight_norm(link):
     hook = chainer.LinkHook()
     hook.forward_preprocess = _replace
     link.add_hook(hook)
-
     return link
 
-class WaveNet(Chain):
-    def __init__(self,n_channels,out_channels,n_layers,cond_channels):
-        dilated_conv=chinaer.ChainList()
-        conditional_conv=chainer.ChainList()
-        residual_conv=chainer.ChainList()
-        skip_conv=chainer.ChainList()
 
-        for layer in range(n_layers):
-            dilated_conv.add_link(weight_norm(L.ConvolutionND(
-                        1,n_channels,n_channels*2,3,pad=2**layer,dilate=2**layer)))
-            conditional_conv.add_link(weight_norm(L.ConvolutionND(
-                        1,n_channels,n_channels*2,1)))
-            residual_conv.add_link(weight_norm(L.ConvolutionND(
-                        1,n_channels,n_channels,1)))
-            skip_conv.add_link(weight_rnom(L.ConvolutionND(
-                        1,n_channels,n_channels,1)))
+class Invertible1x1Convolution(chainer.link.Link):
+    def __init__(self, channel):
+        super(Invertible1x1Convolution, self).__init__()
+        xp = self.xp
 
-        super(WaveNet,self).__init__()
+        W = xp.linalg.qr(xp.random.normal(
+            0, 1, (channel, channel)))[0].astype(xp.float32)
+
+        if xp.linalg.det(W) < 0:
+            W[:, 0] *= -1
+        W = W.reshape(W.shape + (1,))
+
         with self.init_scope():
-            self.cin=weight_norm(L.ConvolutionND(
-                        1,out_channels//2,n_channels,1))
-            self.dilated=dilated_conv
-            self.conditional=conditional_conv
-            self.residual=residual_conv
-            self.skip=skip_conv
-            self.cout=L.ConvolutionND(n_channels,out_channels,1)
+            self.W = chainer.Parameter(W)
 
-    def __call__(self,x,condition):
-        h=self.cin(x)
-        skip_connections=0
-        for dilated,cond,residual,skip in zip(self.dilated,self.conditional,self.residual,self.skip):
-            h=dilated(h)+cond(condition)
-            a,b=F.split(h,2,axis=1)
-            z=F.tanh(a)+F.sigmoid(b)
-            h=residual(z)
-            z=skip(z)
-            skip_connections+=z
-        out=self.cout(skip_connections)
-        logdet,out=F.split(h,2,axis=1)
+    @property
+    def invW(self):
+        return F.expand_dims(F.inv(self.W[..., 0]), axis=2)
 
-        return logdet,out
+    def __call__(self, x):
+        return F.convolution_1d(x, self.W), \
+            x.shape[0] * x.shape[-1] * F.log(F.det(self.W[..., 0]))
 
-class AffineCouplingLayer(Chain):
-    def __init__(self,n_channels,out_channels,n_layers,cond_channels):
-        super(AffineCouplingLayer,self).__init__()
+    def reverse(self, x):
+        return F.convolution_1d(x, self.invW)
+
+
+class WaveNet(chainer.Chain):
+    def __init__(self, out_channel, n_condition, n_layers, n_channel):
+        super(WaveNet, self).__init__()
+        dilated_convs = chainer.ChainList()
+        residual_convs = chainer.ChainList()
+        skip_convs = chainer.ChainList()
+        condition_convs = chainer.ChainList()
+        for i in range(n_layers):
+            dilated_convs.add_link(weight_norm(
+                L.Convolution1D(
+                    n_channel, 2 * n_channel, 3, pad=2 ** i, dilate=2 ** i)))
+            residual_convs.add_link(weight_norm(
+                L.Convolution1D(n_channel, n_channel, 1)))
+            skip_convs.add_link(weight_norm(
+                L.Convolution1D(n_channel, n_channel, 1)))
+            condition_convs.add_link(weight_norm(
+                L.Convolution1D(n_condition, 2 * n_channel, 1)))
         with self.init_scope():
-            self.encoder=WaveNet(n_channels,out_channels,n_layers,cond_channels)
+            self.input_conv = weight_norm(
+                L.Convolution1D(out_channel // 2, n_channel, 1))
+            self.dilated_convs = dilated_convs
+            self.residual_convs = residual_convs
+            self.skip_convs = skip_convs
+            self.condition_convs = condition_convs
+            self.output_conv = L.Convolution1D(
+                n_channel, out_channel, 1,
+                initialW=chainer.initializers.Zero())
 
-    def __call__(self,x,condition):
-        a,b=F.split(x,2,axis=1)
-        logdet,t=self.encoder(a,condition)
-        b=F.exp(logdet)*b+t
+    def __call__(self, x, condition):
+        x = self.input_conv(x)
+        skip_connection = 0
+        for dilated, residual, skip, condition_conv in zip(
+                self.dilated_convs, self.residual_convs, self.skip_convs,
+                self.condition_convs):
+            z = dilated(x) + condition_conv(condition)
+            z_tanh, z_sigmoid = F.split_axis(z, 2, axis=1)
+            z = F.tanh(z_tanh) * F.sigmoid(z_sigmoid)
+            x = residual(z)
+            skip_connection += skip(z)
+        y = self.output_conv(skip_connection)
+        log_s, t = F.split_axis(y, 2, axis=1)
+        return log_s, t
 
-        return F.concat([a,b],axis=1), F.sum(logdet)
 
-    def backward(self,x,condtion):
-        a,b=F.split(x,2,axis=1)
-        logdet,t=self.encoder(a,condition)
-        b=(b-t)/(F.exp(logdet))
-
-        return F.concat([a,b],axis=1)
-
-class Flow(Chain):
-    def __init__(self,n_channels,out_channels,n_layers,cond_channels):
-        super(Flow,self).__init__()
+class AffineCouplingLayer(chainer.Chain):
+    def __init__(self, channels, n_condition, n_layers, wn_channels):
+        super(AffineCouplingLayer, self).__init__()
         with self.init_scope():
-            self.int_conv=InvertibleConvolution(n_channels)
-            self.acl=AffineCouplingLayer(n_channels,out_channels,n_layers,cond_channels)
+            self.encoder = WaveNet(channels, n_condition, n_layers, wn_channels)
 
-    def __call__(self,x,condition):
-        h,log_detW=self.int_conv(x)
-        h,logz=self.acl(h,condition)
+    def __call__(self, x, condition):
+        x_a, x_b = F.split_axis(x, 2, axis=1)
+        log_s, t = self.encoder(x_a, condition)
+        x_b = F.exp(log_s) * x_b + t
+        return F.concat((x_a, x_b), axis=1), F.sum(log_s)
 
-        return h,log_detW,logz
+    def reverse(self, z, condition):
+        x_a, x_b = F.split_axis(z, 2, axis=1)
+        log_s, t = self.encoder(x_a, condition)
+        x_b = (x_b - t) * F.exp(-log_s)
+        return F.concat((x_a, x_b), axis=1)
 
-    def backward(self,x,condition):
-        h=self.int_conv.backward(x)
-        h=self.acl.backward(h,condition)
 
-        return h
-
-def squeeze(x,factor):
-    b,c,l=x.shape
-    h=x.reshape(b,c,int(l/factor),factor).transpose(0,1,3,2)
-
-    return h.reshape(b,c*factor,int(l/factor))
-
-def unsqueeze(x,factor):
-    b,c,l=x.shape
-    h=x.reshape(b,int(c/factor),factor,l).transpose(0,1,3,2)
-
-    return h.reshape(b,int(c/factor),l*factor)
-
-class WaveGlow(Chain):
-    def __init__(self,
-                 hop_length=256,
-                 n_mels=80,
-                 input_channels=1,
-                 scale_factor=8,
-                 n_flow=12,
-                 n_layers=8,
-                 n_channels=512,
-                 early_layer=4,
-                 early_channels=2,
-                 var=0.5):
-        self.hop_length=hop_length
-        self.n_mels=n_mels
-        self.input_channels=1
-        self.n_flows=12
-        self.n_layers=8
-        self.factor=scale_factor
-        self.n_channels=channels
-        self.early_layer=early_layer
-        self.early_channels=early_channels
-        self.var=var
-        self.ln_var=xp.log(var)
-
-        flow=chainer.ChainList()
-        for i in range(n_flows):
-            flow.add_link(Flow(
-                input_channels*scale_factor-early_channels*(i//early_size),
-                n_mels,n_layers,n_channels))
-
-        super(WaveGlow,self).__init__()
+class Flow(chainer.Chain):
+    def __init__(self, channel, n_condition, n_layers, wn_channel):
+        super(Flow, self).__init__()
         with self.init_scope():
-            self.encoder=L.DeconvolutionND(1,n_mels,n_mels,
-                            hop_length*4,hop_length,pad=hop_length*3//2)
-            self.flows=flow
+            self.invertible1x1convolution = Invertible1x1Convolution(
+                channel)
+            self.affinecouplinglayer = AffineCouplingLayer(
+                channel, n_condition, n_layers, wn_channel)
 
-    def __call__(self,x,condition):
-        h=self.encoder(x)
-        h=squeeze(x, self.factor)
-        condition=squeeze(condition, self.factor)
-        sum_log_detW=0
-        sum_logz=0
-        outputs=[]
-        for i,flow in enumerate(self.flows.children()):
-            h,log_detW,logz=flow(h,condtition)
-            if (i+1)%early_layer==0:
-                output,h=h[;,:self.early_channels], h[;,self.early_channels:]
-                outputs.append(output)
-            sum_log_detW+=log_detW
-            sum_logz+=logz
-        outputs.append(h)
-        z=F.concat(outputs,axis=1)
-        gaussian_nll=F.gaussian_nll(
-                z,
-                mean=xp.zeros_like(z).astype(xp.float32),
-                ln_var=self.ln_var*(xp.ones_like(z).astype(xp.float32))
-        )
-        gaussian_nll=gaussian_nll/xp.prod(z.shape)
-        sum_log_detW /= xp.prod(z.shape)
-        sum_logz /= xp.prod(z.shape)
+    def __call__(self, x, condition):
+        x, log_det_W = self.invertible1x1convolution(x)
+        z, log_s = self.affinecouplinglayer(x, condition)
+        return z, log_s, log_det_W
 
-        return z, gaussiah_nll, sum_log_detW, sum_logz
-
-    def backward(self,x,condition,var=0):
-        condition=self.encoder(condition)
-        condition=squeeze(x,self.factor)
-        b,_,l=condition.shape
-        if x is None:
-            x=xp.random.normal(0,1,(b,self.input_channels*self.factor,l)).astype(xp.float32)
-        _, c, _=x.shape
-        s_channels=c-self.early_channels*(self.n_flows//self.early_layer)
-        x,z=x[:, -s_channels:],x[:, :-s_channels]
-        for i,flow in enumerate(reversed(list(self.flows.children()))):
-            if (self.n_flows-i)%self.early_layer:
-                x,z=F.concat((z[;,-self.early_channels:],x)),z[;,:-self.early_channels]
-            x=flow.reverse(x,condition)
-        x=unsqueeze(x,self.factor)
-
+    def reverse(self, z, condition):
+        z = self.affinecouplinglayer.reverse(z, condition)
+        x = self.invertible1x1convolution.reverse(z)
+        
         return x
 
-    def generate(self,condition,var=0.06**2):
-        return self.backward(None,condition,var)
+def _squeeze(x, squeeze_factor):
+    batchsize, channel, length = x.shape
+    x = x.reshape(
+        (batchsize, channel, length // squeeze_factor, squeeze_factor))
+    x = x.transpose((0, 1, 3, 2))
+    x = x.reshape(
+        (batchsize, channel * squeeze_factor, length // squeeze_factor))
+    return x
+
+
+def _unsqueeze(x, squeeze_factor):
+    batchsize, channel, length = x.shape
+    x = x.reshape(
+        (batchsize, channel // squeeze_factor, squeeze_factor, length))
+    x = x.transpose((0, 1, 3, 2))
+    x = x.reshape(
+        (batchsize, channel // squeeze_factor, length * squeeze_factor))
+    return x
+
+
+class Glow(chainer.Chain):
+    def __init__(
+            self, hop_length=256, n_mels=80, input_channel=1,
+            squeeze_factor=8, n_flows=9, n_layers=4,
+            wn_channel=512, early_every=4, early_size=2, var=0.5):
+        super(Glow, self).__init__()
+        self.input_channel = input_channel
+        self.squeeze_factor = squeeze_factor
+        self.n_flows = n_flows
+        self.early_every = early_every
+        self.early_size = early_size
+        self.var = float(var)
+        self.ln_var = float(numpy.log(var))
+        flows = chainer.ChainList()
+        for i in range(n_flows):
+            flows.add_link(Flow(
+                input_channel * squeeze_factor -
+                early_size * (i // early_every),
+                n_mels * squeeze_factor, n_layers, wn_channel))
+        with self.init_scope():
+            self.encoder = chainer.links.Deconvolution1D(
+                n_mels, n_mels, hop_length * 4, hop_length,
+                pad=hop_length * 3 // 2)
+            self.flows = flows
+
+    def __call__(self, x, condition):
+        z, gaussian_nll, sum_log_s, sum_log_det_W = self._forward(x, condition)
+        nll = gaussian_nll - sum_log_s - sum_log_det_W + float(numpy.log(2 ** 16))
+        loss = chainer.functions.mean(z * z / (2 * self.var)) - \
+            sum_log_s - sum_log_det_W
+
+        return loss
+
+    def _forward(self, x, condition):
+        condition = self.encoder(condition)
+        x = _squeeze(x, self.squeeze_factor)
+        condition = _squeeze(condition, self.squeeze_factor)
+        sum_log_s = 0
+        sum_log_det_W = 0
+        outputs = []
+        for i, flow in enumerate(self.flows.children()):
+            x, log_s, log_det_W = flow(x, condition)
+            if (i + 1) % self.early_every == 0:
+                output, x = x[:, :self.early_size], x[:, self.early_size:]
+                outputs.append(output)
+            sum_log_s += log_s
+            sum_log_det_W += log_det_W
+        outputs.append(x)
+        z = chainer.functions.concat(outputs, axis=1)
+        gaussian_nll = chainer.functions.gaussian_nll(
+            z,
+            mean=self.xp.zeros_like(z, dtype=self.xp.float32),
+            ln_var=self.ln_var * self.xp.ones_like(z, dtype=self.xp.float32)
+        )
+        gaussian_nll /= numpy.prod(z.shape)
+        sum_log_s /= numpy.prod(z.shape)
+        sum_log_det_W /= numpy.prod(z.shape)
+        return z, gaussian_nll, sum_log_s, sum_log_det_W
+
+    def _reverse(self, z, condition, var=0):
+        condition = self.encoder(condition)
+        condition = _squeeze(condition, self.squeeze_factor)
+        batchsize, _, length = condition.shape
+        if z is None:
+            z = self.xp.random.normal(
+                0, var,
+                (batchsize, self.input_channel * self.squeeze_factor, length))
+            z = z.astype(self.xp.float32)
+        _, channel, _ = z.shape
+        start_channel = channel - \
+            self.early_size * (self.n_flows // self.early_every)
+        x, z = z[:, -start_channel:], z[:, :-start_channel]
+        for i, flow in enumerate(reversed(list(self.flows.children()))):
+            if (self.n_flows - i) % self.early_every == 0:
+                x, z = chainer.functions.concat((
+                    z[:, -self.early_size:], x)), z[:, :-self.early_size]
+            x = flow.reverse(x, condition)
+        x = _unsqueeze(x, self.squeeze_factor)
+        return x
+
+    def generate(self, condition, var=0.6 ** 2):
+        return self._reverse(None, condition, var)
--- WaveGlow/train.py
+++ WaveGlow/train.py
@@ -5,43 +5,66 @@ from chainer import cuda,Chain,optimizers,serializers
 import numpy as np
 import os
 import argparse
-from model import WaveGlow
-from audio_process import Audio2MelSpectrogram
+from model import Glow
 from librosa.core import load
 from librosa.output import write_wav
+import librosa
 
 xp=cuda.cupy
 cuda.get_device(0).use()
 
-def set_optimizer(model,alpha=0.0001,beta):
+def set_optimizer(model,alpha=0.0001,beta=0.9):
     optimizer=optimizers.Adam(alpha=alpha,beta1=beta)
     optimizer.setup(model)
 
     return optimizer
 
-def data_process(filename):
-    audio,sr=load(filename, sr=16000)
-    rnd=np.random.randint(audio.shape[0]-1024)
-    audio=audio[rnd:rnd+1024]
-    mel=Audio2MelSpectrogram(audio,sr,melsize=80,fftsize=1024,windowsize=1024,windowsshiftsize=256)
+def data_process(filename, length):
+    audio,sr=load(filename)
+    rnd=np.random.randint(audio.shape[0]-length)
+    audio=audio[rnd:rnd+length]
+    
+    mel=librosa.feature.melspectrogram(audio, sr, n_fft=1024, hop_length=256, n_mels=80)
+    mel=(mel / np.max(mel))**0.6
+    mel=mel[:, :int(length/256)]
+    audio = np.expand_dims(np.expand_dims(audio,axis=0), axis=0)
+    mel = np.expand_dims(mel,axis=0)
 
-    return audio,mels
+    return audio,mel
 
-waveglow=WaveGlow()
+parser=argparse.ArgumentParser(description="WaveGlow")
+parser.add_argument("--epochs",default=1000,type=int,help="the number of epochs")
+parser.add_argument("--batchsize",default=16,type=int,help="batch size")
+parser.add_argument("--var",default=0.5,type=float,help="variance")
+parser.add_argument("--interval",default=1,type=int,help="the interval of snapshot")
+parser.add_argument("--iterations",default=2000,type=int,help="the number of iterations")
+parser.add_argument("--length",default=8192*2,type=int,help="the number of iterations")
+
+args=parser.parse_args()
+epochs=args.epochs
+batchsize=args.batchsize
+var=args.var
+interval=args.interval
+iterations=args.iterations
+length=args.length
+
+waveglow=Glow()
 waveglow.to_gpu()
 wg_opt=set_optimizer(waveglow)
 
-audio_path=''
+audio_path='/JSUT/basic5000/wav/'
 audio_list=os.listdir(audio_path)
 
 for epoch in range(epochs):
     sum_loss=0
-    for batch in range(0,Ntrain,batchsize):
+    for batch in range(0,iterations,batchsize):
         audio_name=audio_path+np.random.choice(audio_list)
-        audio,mels=data_process(audio_name)
-        z,gaussian_nll,log_detW,log_z=waveglow(audio,mels)
-        nll=gausiaan_nll-log_detW-log_z+xp.log(2**16)
-        loss=F.mean(z*z/(2*(var**2)))-log_detW-log_z
+        audio,mels=data_process(audio_name, length)
+
+        audio=chainer.as_variable(xp.array(audio).astype(xp.float32))
+        mels=chainer.as_variable(xp.array(mels).astype(xp.float32))
+
+        loss=waveglow(audio,mels)
         
         waveglow.cleargrads()
         loss.backward()
@@ -54,4 +77,4 @@ for epoch in range(epochs):
             serializers.save_npz('waveglow.model',waveglow)
 
     print('epoch:{}'.format(epoch))
-    print('Loss:{}').format(sum_loss/Ntrain)
+    print('Loss:{}'.format(sum_loss/iterations))
