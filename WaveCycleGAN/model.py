3b87ee02f6abe4b1a06400b91e41ddf60403eead
Lento <crosssceneofwindff@gmail.com>
2019-04-15 21:39:00 +0900

    updaate

--- WaveCycleGAN/model.py
+++ WaveCycleGAN/model.py
@@ -3,19 +3,17 @@ import chainer.links as L
 import chainer.functions as F
 from chainer import cuda,Chain,initializers
 import numpy as np
-from instance_normalization import InstanceNormalization
-
 
 xp=cuda.cupy
 cuda.get_device(0).use()
 
 class GLU(Chain):
     def __init__(self):
-        super(GLU,self).__init__()
+        super(GLU, self).__init__()
         
-    def __call__(self,x):
-        a,b=F.split_axis(x,2,axis=1)
-        h=a*F.sigmoid(b)
+    def __call__(self, x):
+        a, b = F.split_axis(x, 2, axis=1)
+        h = a * F.sigmoid(b)
 
         return h
 
@@ -28,20 +26,32 @@ def pixel_shuffler(out_ch, x, r = 2):
     return out_map
 
 class CIGLU(Chain):
-    def __init__(self,in_ch,out_ch,up=False):
-        super(CIGLU,self).__init__()
-        w=initializers.Normal(0.02)
-        self.up=up
+    def __init__(self, in_ch, out_ch, up=False, down=False):
+        super(CIGLU, self).__init__()
+        w = initializers.Normal(0.02)
+        self.up = up
+        self.down = down
         with self.init_scope():
-            self.c0=L.Convolution1D(in_ch,out_ch,60,2,56,initialW=w)
-            self.i0=InstanceNormalization(out_ch)
-            self.glu0=GLU()
+            self.cup = L.Convolution1D(in_ch,out_ch, 59, 1, 29, initialW=w)
+            self.cdown = L.Convolution1D(in_ch, out_ch, 60, 2, 29, initialW=w)
+            self.cpara = L.Convolution1D(in_ch, out_ch, 59, 1, 29, initialW=w)
+
+            self.bn0 = L.BatchNormalization(out_ch)
+            self.glu0 = GLU()
 
     def __call__(self,x):
-        h=self.c0(x)
-        if self.up:
-            h=pixel_shuffler(h)
-        h=self.glu0(self.i0(h))
+        if self.down:
+            h = self.cdown(x)
+            h = self.glu0(self.bn0(h))
+
+        elif self.up:
+            h = F.unpooling_1d(x,2,2,0,cover_all=False)
+            h = self.cpara(h)
+            h = self.glu0(self.bn0(h))
+
+        else:
+            h = self.cpara(x)
+            h=self.glu0(self.bn0(h))
 
         return h
 
@@ -51,7 +61,7 @@ class ResBlock(Chain):
         w=initializers.Normal(0.02)
         with self.init_scope():
             self.ciglu0=CIGLU(in_ch,out_ch)
-            self.ciglu1=CIGLU(out_ch,in_ch)
+            self.ciglu1=CIGLU(in_ch,out_ch)
 
     def __call__(self,x):
         h=self.ciglu0(x)
@@ -62,21 +72,21 @@ class ResBlock(Chain):
 class Generator(Chain):
     def __init__(self,base=32):
         super(Generator,self).__init__()
-        w=initializers.Normal(0.02)
+        w = initializers.Normal(0.02)
         with self.init_scope():
-            self.c0=L.Convolution1D(1,base,60,1,56,initialW=w)
-            self.glu0=GLU()
-            self.ciglu0=CIGLU(base,base*2)
-            self.ciglu1=CIGLU(base*2,base)
-            self.res0=ResBlock(base,base*2)
-            self.res1=ResBlock(base,base*2)
-            self.res2=ResBlock(base,base*2)
-            self.res3=ResBlock(base,base*2)
-            self.res4=ResBlock(base,base*2)
-            self.res5=ResBlock(base,base*2)
-            self.ciglu2=CIGLU(base,base,up=True)
-            self.ciglu3=CIGLU(base,base*2,up=True)
-            self.c1=L.Convolution1D(base*2,1,60,1,56,initialW=w)
+            self.c0 = L.Convolution1D(1, base, 59, 1, 29, initialW=w)
+            self.glu0 = GLU()
+            self.ciglu0 = CIGLU(int(base/2), base*2, down=True)
+            self.ciglu1 = CIGLU(base, base*2, down=True)
+            self.res0 = ResBlock(base, base*2)
+            self.res1 = ResBlock(base, base*2)
+            self.res2 = ResBlock(base, base*2)
+            self.res3 = ResBlock(base, base*2)
+            self.res4 = ResBlock(base, base*2)
+            self.res5 = ResBlock(base, base*2)
+            self.ciglu2 = CIGLU(base, base*2,up=True)
+            self.ciglu3 = CIGLU(base, base*2,up=True)
+            self.c1 = L.Convolution1D(base, 1, 59, 1, 29,initialW=w)
 
     def __call__(self,x):
         h=self.glu0(self.c0(x))
@@ -99,11 +109,11 @@ class Discriminator(Chain):
         super(Discriminator,self).__init__()
         w=initializers.Normal
         with self.init_scope():
-            self.c0=L.Convolution1D(1,base,60,1,56)
+            self.c0=L.Convolution1D(1, base*2, 59, 1, 29)
             self.glu=GLU()
-            self.ciglu0=CIGLU(base,base*2)
-            self.ciglu1=CIGLU(base*2,base)
-            self.l0=L.Linear(None,1,initialW=w)
+            self.ciglu0=CIGLU(base, base*2)
+            self.ciglu1=CIGLU(base, base*2)
+            self.l0=L.Linear(None, 1, initialW=w)
 
     def __call__(self,x):
         h=self.glu(self.c0(x))
