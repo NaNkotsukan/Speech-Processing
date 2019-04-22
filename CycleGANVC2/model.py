f64658d1e3812dc4b527552a2c825352c4e848aa
Lento <crosssceneofwindff@gmail.com>
2019-04-22 22:27:07 +0900

    on experiment

---
+++ CycleGANVC2/model.py
@@ -0,0 +1,151 @@
+import chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import cuda, Chain, initializers
+import numpy as np
+
+xp = cuda.cupy
+cuda.get_device(0).use()
+
+def glu(x):
+    a, b = F.split_axis(x, 2, axis=1)
+    
+    return a * F.sigmoid(b)
+
+class C2BG(Chain):
+    def __init__(self, in_ch, out_ch, up=False, down=False):
+        super(C2BG, self).__init__()
+        w = initializers.Normal(0.02)
+        self.up = up
+        self.down = down
+        with self.init_scope():
+            self.cup = L.Convolution2D(in_ch, out_ch,3,1,1,initialW=w)
+            self.cpara = L.Convolution2D(in_ch, out_ch,3,1,1,initialW=w)
+            self.cdown = L.Convolution2D(in_ch, out_ch, 4,2,1,initialW=w)
+
+            self.bn0 = L.BatchNormalization(out_ch)
+
+    def __call__(self, x):
+        if self.up:
+            h = F.unpooling_2d(x, 2, 2, 0, cover_all=False)
+            h = glu(self.bn0(self.cup(h)))
+
+        elif self.down:
+            h = glu(self.bn0(self.cdown(x)))
+
+        else:
+            h = glu(self.bn0(self.cpara(x)))
+
+        return h
+
+class C1BG(Chain):
+    def __init__(self, in_ch, out_ch, up=False, down=False):
+        super(C1BG, self).__init__()
+        w = initializers.Normal(0.02)
+        self.up = up
+        self.down = down
+        with self.init_scope():
+            self.cup = L.Convolution1D(in_ch, out_ch,3,1,1,initialW=w)
+            self.cpara = L.Convolution1D(in_ch, out_ch,3,1,1,initialW=w)
+            self.cdown = L.Convolution1D(in_ch, out_ch, 4,2,1,initialW=w)
+
+            self.bn0 = L.BatchNormalization(out_ch)
+
+    def __call__(self, x):
+        if self.up:
+            h = F.unpooling_1d(x, 2, 2, 0, cover_all=False)
+            h = glu(self.bn0(self.cup(h)))
+
+        if self.down:
+            h = glu(self.bn0(self.cdown(x)))
+
+        else:
+            h = glu(self.bn0(self.cpara(x)))
+
+        return h
+
+class ResBlock(Chain):
+    def __init__(self, in_ch, out_ch):
+        w = initializers.Normal(0.02)
+        super(ResBlock, self).__init__()
+        with self.init_scope():
+            self.cbg0 = C1BG(in_ch, out_ch)
+            self.c0 = L.Convolution1D(in_ch, in_ch, 3,1,1,initialW=w)
+            self.bn0 = L.BatchNormalization(in_ch)
+
+    def __call__(self, x):
+        h = self.cbg0(x)
+        h = self.bn0(self.c0(h))
+
+        return h + x
+
+class Generator(Chain):
+    def __init__(self, base=128):
+        w = initializers.Normal(0.02)
+        super(Generator, self).__init__()
+        with self.init_scope():
+            self.c0 = L.Convolution2D(1, base, (15, 5), 1, (7, 2), initialW=w)
+            self.cbg0 = C2BG(int(base/2), base*2, down=True)
+            self.cbg1 = C2BG(base, base*4, down=True)
+            
+            self.c1 = L.Convolution1D(2304, base*2, 1, 1, 0, initialW=w)
+            self.bn1 = L.BatchNormalization(base*2)
+
+            self.res0 = ResBlock(base*2, base*4)
+            self.res1 = ResBlock(base*2, base*4)
+            self.res2 = ResBlock(base*2, base*4)
+            self.res3 = ResBlock(base*2, base*4)
+            self.res4 = ResBlock(base*2, base*4)
+            self.res5 = ResBlock(base*2, base*4)
+
+            self.c2 = L.Convolution1D(base*2, 2304, 1, 1, 0, initialW=w)
+            self.bn2 = L.BatchNormalization(2304)
+
+            self.cbg2 = C2BG(base*2, base*8, up=True)
+            self.cbg3 = C2BG(base*4, 72, up=True)
+            
+            self.c3 = L.Convolution2D(36, 1, 3, 1, 1, initialW=w)
+
+    def __call__(self, x):
+        b = x.shape[0]
+        h = glu(self.c0(x))
+        h = self.cbg0(h)
+        h = self.cbg1(h)
+        h = F.transpose(h, (0, 1, 3, 2)).reshape(b, 2304, 32)
+        h = self.bn1(self.c1(h))
+        h = self.res0(h)
+        h = self.res1(h)
+        h = self.res2(h)
+        h = self.res3(h)
+        h = self.res4(h)
+        h = self.res5(h)
+        h = self.bn2(self.c2(h))
+        h = F.transpose(F.reshape(h, (b, 256, 9, 32)), (0, 1, 3, 2))
+        h = self.cbg2(h)
+        h = self.cbg3(h)
+        h = self.c3(h)
+
+        return h
+
+class Discriminator(Chain):
+    def __init__(self,base=64):
+        w = initializers.Normal(0.02)
+        super(Discriminator, self).__init__()
+
+        with self.init_scope():
+            self.c0 = L.Convolution2D(1, base*2, 3, 1, 1, initialW=w)
+            self.cbg0 = C2BG(base, base*4, down=True)
+            self.cbg1 = C2BG(base*2, base*8, down=True)
+            self.cbg2 = C2BG(base*4, base*16, down=True)
+            self.cbg3 = C2BG(base*8, base*16, down=True)
+            self.c1 = L.Convolution2D(base*8, 1, 3, 1, 1, initialW=w)
+
+    def __call__(self, x):
+        h = glu(self.c0(x))
+        h = self.cbg0(h)
+        h = self.cbg1(h)
+        h = self.cbg2(h)
+        h = self.cbg3(h)
+        h = self.c1(h)
+
+        return h
---
+++ CycleGANVC2/train.py
@@ -0,0 +1,179 @@
+import chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import cuda, Chain, optimizers, serializers
+import numpy as np
+import os
+import argparse
+import pylab
+from model import Generator, Discriminator
+from lain.audio.navi import load, save
+from lain.audio.layer.converter import audio2af, encode_sp
+from lain.audio.layer.augmentation import random_crop
+from librosa.display import specshow
+
+xp = cuda.cupy
+cuda.get_device(0).use()
+
+def set_optimizer(model, alpha=0.0002, beta1=0.5):
+    optimizer = optimizers.Adam(alpha=alpha, beta1=beta1)
+    optimizer.setup(model)
+
+    return optimizer
+
+def normalize(x, epsilon=1e-8):
+    x_mean, x_std = np.mean(x + epsilon), np.std(x + epsilon)
+
+    return (x - x_mean) / x_std
+
+parser = argparse.ArgumentParser(description='CycleGAN-VC2')
+parser.add_argument('--epoch', default=1000, type=int, help="the number of epochs")
+parser.add_argument('--batchsize', default=3, type=int, help="batch size")
+parser.add_argument('--testsize', default=2, type=int, help="test size")
+parser.add_argument('--Ntrain', default=5000, type=int, help="data size")
+parser.add_argument('--cw', default=10.0, type=float, help="the weight of cycle loss")
+parser.add_argument('--iw', default=5.0, type=float, help="the weight of identity loss")
+
+args = parser.parse_args()
+epochs = args.epoch
+batchsize = args.batchsize
+testsize = args.testsize
+Ntrain = args.Ntrain
+cycle_weight = args.cw
+identity_weight = args.iw
+
+x_path ='./Dataset/Speech/JSUT/basic5000/wav_16/'
+x_list = os.listdir(x_path)
+x_len = len(x_list)
+y_path = './Dataset/Speech/kaede_tmp/'
+y_list = os.listdir(y_path)
+y_len = len(y_list)
+
+generator_xy = Generator()
+generator_xy.to_gpu()
+gen_xy_opt = set_optimizer(generator_xy)
+
+generator_yx = Generator()
+generator_yx.to_gpu()
+gen_yx_opt = set_optimizer(generator_yx)
+
+discriminator_y = Discriminator()
+discriminator_y.to_gpu()
+dis_y_opt = set_optimizer(discriminator_y, alpha=0.0001)
+
+discriminator_x = Discriminator()
+discriminator_x.to_gpu()
+dis_x_opt = set_optimizer(discriminator_x, alpha=0.0001)
+
+discriminator_xyx = Discriminator()
+discriminator_xyx.to_gpu()
+dis_xyx_opt = set_optimizer(discriminator_xyx, alpha=0.0001)
+
+discriminator_yxy = Discriminator()
+discriminator_yxy.to_gpu()
+dis_yxy_opt = set_optimizer(discriminator_yxy, alpha=0.0001)
+
+for epoch in range(epochs):
+    sum_gen_loss = 0
+    sum_dis_loss = 0
+    for batch in range(0, Ntrain, batchsize):
+        x_sp_box = []
+        y_sp_box = []
+        for _ in range(batchsize):
+            rnd_x = np.random.randint(x_len)
+            xx = load(x_path + x_list[rnd_x], sampling_rate=16000)
+            rnd_y = np.random.randint(y_len)
+            yy = load(y_path + y_list[rnd_y], sampling_rate=16000)
+            xx = random_crop(xx, upper_bound=10200)
+            yy = random_crop(yy, upper_bound=10200)
+            _, sp_x, _ = audio2af(xx)
+            _, sp_y, _ = audio2af(yy)
+
+            sp_x = normalize(encode_sp(sp_x))
+            sp_y = normalize(encode_sp(sp_y))
+
+            x_sp_box.append(sp_x[np.newaxis,:])
+            y_sp_box.append(sp_y[np.newaxis,:])
+
+        x = chainer.as_variable(xp.array(x_sp_box).astype(xp.float32))
+        y = chainer.as_variable(xp.array(y_sp_box).astype(xp.float32))
+
+        xy = generator_xy(x)
+        #xyx = generator_yx(xy)
+
+        yx = generator_yx(y)
+        #yxy = generator_xy(yx)
+
+        xy.unchain_backward()
+        #xyx.unchain_backward()
+        yx.unchain_backward()
+        #yxy.unchain_backward()
+
+        dis_y_fake = discriminator_y(xy)
+        dis_y_real = discriminator_y(y)
+        dis_x_fake = discriminator_x(yx)
+        dis_x_real = discriminator_x(x)
+
+        #dis_xx_fake = discriminator_xyx(xyx)
+        #dis_xx_real = discriminator_xyx(x)
+        #dis_yy_fake = discriminator_yxy(yxy)
+        #dis_yy_real = discriminator_yxy(y)
+
+        dis_loss = F.mean(F.softplus(-dis_y_fake)) + F.mean(F.softplus(dis_y_real))
+        dis_loss += F.mean(F.softplus(-dis_x_fake)) + F.mean(F.softplus(dis_x_real))
+        #dis_loss += F.mean(F.softplus(-dis_xx_fake)) + F.mean(F.softplus(dis_xx_real))
+        #dis_loss += F.mean(F.softplus(-dis_yy_fake)) + F.mean(F.softplus(dis_yy_real))
+
+        discriminator_x.cleargrads()
+        discriminator_y.cleargrads()
+        #discriminator_xyx.cleargrads()
+        #discriminator_yxy.cleargrads()
+        dis_loss.backward()
+        dis_x_opt.update()
+        dis_y_opt.update()
+        #dis_xyx_opt.update()
+        #dis_yxy_opt.update()
+        dis_loss.unchain_backward()
+
+        xy = generator_xy(x)
+        xyx = generator_yx(xy)
+        id_y = generator_xy(y)
+
+        yx = generator_yx(y)
+        yxy = generator_xy(yx)
+        id_x = generator_yx(x)
+
+        dis_y_fake = discriminator_y(xy)
+        dis_x_fake = discriminator_x(yx)
+        #dis_xx_fake = discriminator_xyx(xyx)
+        #dis_yy_fake = discriminator_yxy(yxy)
+
+        cycle_loss_x = F.mean_absolute_error(x, xyx)
+        cycle_loss_y = F.mean_absolute_error(y, yxy)
+        cycle_loss = cycle_loss_x + cycle_loss_y
+        
+        identity_loss_x = F.mean_absolute_error(id_y, y)
+        identity_loss_y = F.mean_absolute_error(id_x, x)
+        identity_loss = identity_loss_x + identity_loss_y
+
+        gen_loss = F.mean(F.softplus(-dis_x_fake)) + F.mean(F.softplus(-dis_y_fake))
+        #gen_loss += F.mean(F.softplus(-dis_xx_fake)) + F.mean(F.softplus(-dis_yy_fake))
+        gen_loss += cycle_weight * cycle_loss + identity_weight * identity_loss
+
+        generator_xy.cleargrads()
+        generator_yx.cleargrads()
+        gen_loss.backward()
+        gen_xy_opt.update()
+        gen_yx_opt.update()
+        gen_loss.unchain_backward()
+
+        sum_dis_loss += dis_loss.data.get()
+        sum_gen_loss += gen_loss.data.get()
+
+        if batch == 0:
+            serializers.save_npz('generator_xy.model', generator_xy)
+            serializers.save_npz('generator_yx.model', generator_xy)
+
+    print('epoch : {}'.format(epoch))
+    print('Generator loss : {}'.format(sum_gen_loss / x_len))
+    print('Discriminator loss : {}'.format(sum_dis_loss / x_len))
